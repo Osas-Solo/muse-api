8bca4f99fd24b98f55692ef40b1594f93f6b169b
Osaremhen Ukpebor <osas.solo@outlook.com>
2022-10-11 12:04:36 +0100

    Added subscription types API

--- api/user.js
+++ api/user.js
@@ -20,13 +20,7 @@ exports.getSubscriptionTypes = (request, response) => {
             };
 
             for (const currentSubscriptionType of results) {
-                let currentSubscriptionTypeJSON = {
-                    subscriptionTypeID: currentSubscriptionType.subscription_type_id,
-                    subscriptionCode: currentSubscriptionType.subscription_code,
-                    subscriptionName: currentSubscriptionType.subscription_name,
-                    price: currentSubscriptionType.price,
-                    numberOfSongs: currentSubscriptionType.number_of_songs,
-                };
+                let currentSubscriptionTypeJSON = getCurrentSubscriptionTypeJSON(currentSubscriptionType);
 
                 responseJSON.subscriptionTypes.push(currentSubscriptionTypeJSON);
             }
@@ -38,3 +32,51 @@ exports.getSubscriptionTypes = (request, response) => {
         });
     });
 };
+
+exports.getSubscriptionTypeByID = (request, response) => {
+    const subscriptionTypeID = request.params.id;
+
+    database.getConnection(function (err, connection) {
+        if (err) throw err;
+
+        const subscriptionTypeQuery = `SELECT * FROM subscription_types WHERE subscription_type_id = ${subscriptionTypeID}`;
+
+        connection.query(subscriptionTypeQuery, function (error, results, fields) {
+            if (err) {
+                response.status(500).json("Internal server error");
+                throw err;
+            }
+
+            if (results.length == 0) {
+                const responseJSON = {
+                    status: 404,
+                    error: `Sorry, no subscription type with the ID: ${subscriptionTypeID} could be found`,
+                };
+
+                response.status(404).send(responseJSON);
+            } else {
+                const responseJSON = {
+                    status: 200,
+                    subscriptionType: getCurrentSubscriptionTypeJSON(results[0]),
+                };
+
+                response.status(200).send(responseJSON);
+            }
+
+            connection.release();
+            if (error) throw error;
+        });
+    });
+};
+
+function getCurrentSubscriptionTypeJSON(currentSubscriptionType) {
+    let currentSubscriptionTypeJSON = {
+        subscriptionTypeID: currentSubscriptionType.subscription_type_id,
+        subscriptionCode: currentSubscriptionType.subscription_code,
+        subscriptionName: currentSubscriptionType.subscription_name,
+        price: currentSubscriptionType.price,
+        numberOfSongs: currentSubscriptionType.number_of_songs,
+    };
+
+    return currentSubscriptionTypeJSON;
+}
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,6 @@ const userAPI = require("../api/user");
 const router = express.Router();
 
 router.get("/subscription-types", userAPI.getSubscriptionTypes);
+router.get("/subscription-types/:id", userAPI.getSubscriptionTypeByID);
 
 module.exports = router;
