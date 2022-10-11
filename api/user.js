fa192d52fc7985ac9f2d214bc927862e65a3b87a
Osaremhen Ukpebor <osas.solo@outlook.com>
2022-10-11 11:45:04 +0100

    Added subscription types API

--- api/user.js
+++ api/user.js
@@ -2,12 +2,36 @@ const express = require("express");
 const database = require("../config/database");
 
 exports.getSubscriptionTypes = (request, response) => {
-    database.getConnection(function(err, connection) {
+    database.getConnection(function (err, connection) {
         if (err) throw err;
 
-        connection.query("SELECT * FROM subscription_types", function (error, results, fields) {
+        const subscriptionTypesQuery = "SELECT * FROM subscription_types ORDER BY subscription_type_id";
 
-            response.send(results);
+        connection.query(subscriptionTypesQuery, function (error, results, fields) {
+            if (err) {
+                response.status(500).json("Internal server error");
+                throw err;
+            }
+
+            let responseJSON = {
+                status: 200,
+                subscriptionTypes: [],
+                total: results.length,
+            };
+
+            for (const currentSubscriptionType of results) {
+                let currentSubscriptionTypeJSON = {
+                    subscriptionTypeID: currentSubscriptionType.subscription_type_id,
+                    subscriptionCode: currentSubscriptionType.subscription_code,
+                    subscriptionName: currentSubscriptionType.subscription_name,
+                    price: currentSubscriptionType.price,
+                    numberOfSongs: currentSubscriptionType.number_of_songs,
+                };
+
+                responseJSON.subscriptionTypes.push(currentSubscriptionTypeJSON);
+            }
+
+            response.status(200).send(responseJSON);
 
             connection.release();
             if (error) throw error;
