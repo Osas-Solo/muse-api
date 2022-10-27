34c23456c071ec93f82641f25a517ad0cfe1857b
Osaremhen Ukpebor <osas.solo@outlook.com>
2022-10-27 18:30:55 +0100

    Added user's subscription payment API

--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,7 @@ router.post("/users/signup", userAPI.signup);
 router.post("/users/login", userAPI.login);
 router.get("/users/:userID", userAPI.getUserProfile);
 router.get("/users/:userID/subscriptions", userAPI.getSubscriptions);
+router.post("/users/:userID/subscriptions", userAPI.paySubscription);
 router.get("/users/:userID/subscriptions/:id", userAPI.getSubscriptionByID);
 
 module.exports = router;
