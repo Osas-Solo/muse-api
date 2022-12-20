9a4afd6987b4d3d2ff601ef65bdd31e83570e81f
Osaremhen Ukpebor <osas.solo@outlook.com>
2022-12-20 21:34:10 +0100

    Updated database config

--- config/database.js
+++ config/database.js
@@ -3,10 +3,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const databaseConfig = {
+    url: process.env.DATABASE_URL,
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
+    database: process.env.DATABASE_NAME,
+    ssl: {}
 };
 
 const database = mysql.createPool(databaseConfig);
